ea63d516cc73080183611d164c9e9fdd34fede9d
sengokyu <sengokyu+gh@gmail.com>
2021-04-12 11:38:22 +0900

    Rewrite dashboard component spec

--- src/app/dashboard/dashboard.component.spec.ts
+++ src/app/dashboard/dashboard.component.spec.ts
@@ -1,33 +1,20 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { render, RenderResult } from '@testing-library/angular';
 import { of } from 'rxjs';
-import { HeroSearchComponent } from '../hero-search/hero-search.component';
 import { HeroService } from '../hero.service';
 import { HEROES } from '../mock-heroes';
 import { DashboardComponent } from './dashboard.component';
 
 describe('DashboardComponent', () => {
-  let component: DashboardComponent;
-  let fixture: ComponentFixture<DashboardComponent>;
+  let component: RenderResult<DashboardComponent, DashboardComponent>;
   let heroService;
   let getHeroesSpy;
 
-  beforeEach(
-    waitForAsync(() => {
-      heroService = jasmine.createSpyObj('HeroService', ['getHeroes']);
-      getHeroesSpy = heroService.getHeroes.and.returnValue(of(HEROES));
-      TestBed.configureTestingModule({
-        declarations: [DashboardComponent, HeroSearchComponent],
-        imports: [RouterTestingModule.withRoutes([])],
-        providers: [{ provide: HeroService, useValue: heroService }],
-      }).compileComponents();
-    })
-  );
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(DashboardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  beforeEach(async () => {
+    heroService = jasmine.createSpyObj('HeroService', ['getHeroes']);
+    getHeroesSpy = heroService.getHeroes.and.returnValue(of(HEROES));
+    component = await render(DashboardComponent, {
+      providers: [{ provide: HeroService, useValue: heroService }],
+    });
   });
 
   it('should be created', () => {
@@ -35,22 +22,16 @@ describe('DashboardComponent', () => {
   });
 
   it('should display "Top Heroes" as headline', () => {
-    expect(fixture.nativeElement.querySelector('h2').textContent).toEqual(
-      'Top Heroes'
-    );
+    // h2の中のテキストを取得
+    // 取得できなければ例外が発生するので、expectしなくてよい
+    component.getByText('Top Heroes', { selector: 'h2' });
   });
 
-  it(
-    'should call heroService',
-    waitForAsync(() => {
-      expect(getHeroesSpy.calls.any()).toBe(true);
-    })
-  );
+  it('should call heroService', () => {
+    expect(getHeroesSpy.calls.any()).toBe(true);
+  });
 
-  it(
-    'should display 4 links',
-    waitForAsync(() => {
-      expect(fixture.nativeElement.querySelectorAll('a').length).toEqual(4);
-    })
-  );
+  it('should display 4 links', () => {
+    expect(component.queryAllByText(/./, { selector: 'a' }).length).toEqual(4);
+  });
 });
